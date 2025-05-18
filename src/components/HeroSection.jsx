90b4e2aa7e0a34b93baa5ea7a348f6aeea05ab65
ChenMyae KayKhaing <112561446+Chen-Myae2K@users.noreply.github.com>
2025-05-18 19:37:02 +0800

    feat: hero section done

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Letter } from "./animation/TextRandomized";
+import { FlipWords } from "./animation/flip-words";
+import { TypingAnimation } from "@/components/magicui/typing-animation";
+
+const HeroSection = () => {
+  const text = "Chen Myae Kay Khaing";
+  const words = ["Frontend", "Backend", "Mobile"];
+  return (
+    <section className="h-screen overflow-x-hidden bg-gradient-to-t from-[#FF6774] via-[#FF8F9B] to-[#FFB4BE] relative">
+      <div className="w-[90%] mx-auto">
+        <div className=" absolute bottom-3">
+          <div className="text-xl text-semibold w-full lg:w-2/3 lg:text-[35px] leading-11 text-white/80 font-manrope ">
+            Meet your trusted <FlipWords words={words} /> developer, building
+            dynamic digital experiences for web apps, SaaS, websites, and mobile
+            applications.
+          </div>
+          <h1 className="font-manrope  font-extrabold leading-16  lg:mb-0 lg:leading-tight text-[12vw] lg:text-[8vw] text-nowrap text-white text-whitepointer-events-none whitespace-pre-wrap lg:bg-gradient-to-b  from-9c81ff to-white bg-clip-text lg:text-transparent">
+            {/* {text.split("").map((letter, index) => {
+              return <Letter letter={letter} key={`${index}-${letter}`} />;
+            })} */}
+            <TypingAnimation>Chen Myae Kay Khaing</TypingAnimation>
+          </h1>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
